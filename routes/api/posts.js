d901d26008230a5e2b96237803ece69d47919cec
Taylor <toreilly317@gmail.com>
2018-09-05 22:56:42 -0400

    start post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-router.get("/", (req, res) => {
-  res.json({ msg: "post route works" });
-});
+const Post = require("../../models/Post");
+
+/* 
+@route POST api/posts
+@desc Create new post
+@access Private
+ */
+
+router.get(
+  "/create",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { text, name, profileImage } = { ...req.body };
+    const newPost = new Post({
+      text,
+      name,
+      profileImage,
+      user: req.user.id
+    });
+
+    const post = newPost
+      .save()
+      .then(post => {
+        res.json({ post });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+);
 
 module.exports = router;
